884f70abc6cf3ac64f79f4d9dfe8d901176c3a9f
James Grant <j.r.grant@liverpool.ac.uk>
2024-01-07 00:44:57 +0200

    fix

--- environment_setup/install_dependencies.sh
+++ environment_setup/install_dependencies.sh
@@ -1,24 +1,6 @@
 pip install -r requirements.txt
 
 #install graphviz
-#which distro are we on?
-distro=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
-echo $distro
-#install graphviz
-if [ "$distro" == "\"Ubuntu\"" ]; then
-    echo "Installing graphviz on Ubuntu"
-    sudo apt-get install graphviz
-elif [ "$distro" == "\"CentOS Linux\"" ]; then
-    echo "Installing graphviz on CentOS"
-    sudo yum install graphviz
-elif [ "$distro" == "\"Fedora\"" ]; then
-    echo "Installing graphviz on Fedora"
-    sudo dnf install graphviz
-elif [ "$distro" == "\"Mac OS X\"" ]; then
-    echo "Installing graphviz on Mac"
-    brew install graphviz
-else
-    echo "Installing graphviz on Windows"
-    conda install graphviz
-fi
+sudo apt-get install graphviz -y
+conda install graphviz -y
 
